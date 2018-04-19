79afb1b35efe0b27de09ad0f37a5a749d196a869
conanxjp <dianxu320@gmail.com>
2018-04-19 09:52:49 -0700

    add word cloud prototype

--- UI/map/assets/js/consumer_app.js
+++ UI/map/assets/js/consumer_app.js
@@ -490,6 +490,45 @@ function addFilterSliders() {
           .attr('class', 'slider');
 }
 
+
+function drawWordCloud() {
+  var fill = d3.scaleOrdinal(d3.schemeCategory20);
+  var layout = d3.layout.cloud()
+                  .size([200, 200])
+                    .words([
+    "Hello", "world", "normally", "you", "want", "more", "words",
+    "than", "this"].map(function(d) {
+      return {text: d, size: 10 + Math.random() * 10, test: "haha"};
+    }))
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+
+  layout.start();
+
+  function draw(words) {
+    d3.select("#good_text_cloud").append("svg")
+    .attr("width", layout.size()[0])
+    .attr("height", layout.size()[1])
+    .append("g")
+    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+    .data(words)
+    .enter().append("text")
+    .style("font-size", function(d) { return d.size + "px"; })
+    .style("font-family", "Impact")
+    .style("fill", function(d, i) { return fill(i); })
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    })
+    .text(function(d) { return d.text; });
+  }
+}
+
+
 // initialized funciton
 function init() {
   stateList.selectAll('option')
@@ -511,6 +550,11 @@ states = L.geoJson(statesData, {
 }).addTo(map);  // add the configured GeoJson layer to map
 
 init();
+
+drawWordCloud();
+
+
+
 
 // var cityCenters = [];
 // for (var i in cities._layers) {
